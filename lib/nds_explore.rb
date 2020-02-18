5f1615c5bd9ae6cfdd23ef67e1727611821bfd6d
Joshua Gauthreaux <joshuagauthreaux@gmail.com>
2020-02-18 22:11:06 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -14,8 +14,8 @@ pretty_print_nds directors_database
 
 def print_first_directors_movie_titles
   row_index = 0 
-  while row_index < directors_database[0][:name][0][:movies].count do
-    puts directors_database[0][:name][0][:movies][:title].to_s
+  while row_index < directors_database[0][:name][:movies][0].count do
+    puts directors_database[0][:name][:movies][0][:title]
     row_index += 1
     
     
